3c3d2f597affdc6276a3e611519b312dc6a406ae
jamilaferron <jamilaferron@me.com>
2024-09-28 22:19:11 +0100

    refined game play loop

--- main.py
+++ main.py
@@ -29,53 +29,65 @@ game_over = False
 inventory = []
 current_room = kitchen
 
-  # Game loop
+# Game loop
 while not game_over:
   current_room.get_details()
 
+  # Check if there's a character in the room
   inhabitant = current_room.get_character()
   if inhabitant is not None:
-    inhabitant.describe()
-  
+      inhabitant.describe()
+
   # Check if there's an item in the room
   item = current_room.get_item()
   if item:
       print(f"You see a {item.get_name()} here.")
-    
+
+  # Get player input
   command = input("What do you want to do? ").lower()
-    
 
   if command in ['north', 'south', 'east', 'west']:
-
-    if command in current_room.get_linked_rooms():
-      current_room = current_room.move(command)
-    else:
-      print("You can't go that way.")
+      # Move to another room if possible
+      linked_rooms = current_room.get_linked_rooms()
+      if command in linked_rooms:
+          current_room = current_room.move(command)
+      else:
+          print("You can't go that way.")
 
   elif command.startswith('pick up'):
-        # Attempt to pick up the item in the room
-        if item and command == f'pick up {item.get_name()}':
-            if item.get_name() in ['sword', 'cheese']:
-                inventory.append(item)
-                print(f"You picked up the {item.get_name()}.")
-                current_room.set_item(None)  # Remove the item from the room after picking it up
-        else:
-            print("There is no such item here.")
-  elif command == 'fight':
-      if inhabitant and len(inventory) > 0:
-        print(inventory)
-        print(inhabitant.__dict__)
-        weapon = input(f"What will you fight {inhabitant.get_name()} with?: ")
-        if weapon in ['sword', 'cheese']:
-          has_won = inhabitant.fight(weapon)
-          if has_won:
-            current_room.set_character(None)
+      # Attempt to pick up the item in the room
+      if item:
+          if command == f'pick up {item.get_name()}':
+              inventory.append(item)
+              print(f"You picked up the {item.get_name()}.")
+              current_room.set_item(None)  # Remove the item from the room after picking it up
           else:
-             game_over = True
-        else:
-           print(f"you do not have a {weapon} in your inventory" )
+              print(f"You can't pick up a {command[8:]} here.")
       else:
-          print("There's no enemy here or you have no weapon.")
-  else:
-        print("I don't understand that command.")
+          print("There is no such item here.")
 
+  elif command == 'fight':
+      # Handle fight logic
+      if inhabitant:
+          if len(inventory) > 0:
+              print(inventory)
+              weapon = input(f"What will you fight {inhabitant.get_name()} with?: ").lower()
+              
+              # Check if the weapon is in the player's inventory
+              if weapon in [item.get_name() for item in inventory]:
+                  has_won = inhabitant.fight(weapon)
+                  if has_won:
+                      print(f"You defeated {inhabitant.get_name()}!")
+                      current_room.set_character(None)  # Remove the character after defeating them
+                  else:
+                      print(f"{inhabitant.get_name()} defeated you!")
+                      game_over = True
+              else:
+                  print(f"You don't have a {weapon} in your inventory.")
+          else:
+              print("You have no weapon to fight with.")
+      else:
+          print("There's no enemy here.")
+
+  else:
+      print("I don't understand that command.")
